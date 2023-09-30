da96cb7eb781a9e457ef9dcce309626fa33508da
Vítor Falci <falcivitor@gmail.com>
2023-09-30 14:34:43 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Header</h1>
+      </header>
+      <body>
+        <h1>Body</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
